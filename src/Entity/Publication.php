bc0bbe1a9818502643328145439f80be62a1ad4c
hayem <marc.haye@etu.umontpellier.fr>
2025-11-12 13:06:13 +0100

    add structure

---
+++ src/Entity/Publication.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\PublicationRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiProperty;
+
+#[ApiResource(
+    order : ["chiffreAffaire" => "DESC"],
+    operations: [
+        new Get(),
+        new Post(),
+        new Delete(),
+    ]
+)]
+#[ORM\Entity(repositoryClass: PublicationRepository::class)]
+#[ORM\HasLifecycleCallbacks]
+class Publication
+{
+    #[ApiProperty(description: 'Contenu de la publication', writable: true)]
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[Assert\NotNull]
+    #[Assert\NotBlank]
+    #[Assert\Length(
+        min: 4,
+        max: 50,
+        minMessage: "Le message est trop court! (4 caractères minimum)",
+        maxMessage: "Le message est trop long! (50 caractères maximum)"
+     )]
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $message = null;
+
+    #[ApiProperty(description: 'Date de création de la publication', writable: false)]
+    #[ORM\Column]
+    private ?\DateTime $datePublication = null;
+
+    #[ORM\PrePersist]
+    public function prePersistDatePublication() : void {
+        $this->datePublication = new \DateTime();
+    }
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): static
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    public function getDatePublication(): ?\DateTime
+    {
+        return $this->datePublication;
+    }
+
+    public function setDatePublication(\DateTime $datePublication): static
+    {
+        $this->datePublication = $datePublication;
+
+        return $this;
+    }
+}
